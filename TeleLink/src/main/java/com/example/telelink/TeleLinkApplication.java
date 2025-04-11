d47c1905ee6a3ae86d55eb00204ae31edb271a03
Christian Flores <167637198+Cjfs2005@users.noreply.github.com>
2025-04-11 12:16:17 -0500

    Creación del proyecto

---
+++ TeleLink/src/main/java/com/example/telelink/TeleLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.telelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeleLinkApplication.class, args);
+    }
+
+}
---
+++ TeleLink/src/test/java/com/example/telelink/TeleLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
