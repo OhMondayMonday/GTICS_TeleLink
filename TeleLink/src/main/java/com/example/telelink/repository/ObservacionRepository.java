6828833bf202e6374b8825fef9d12b4d994c6bcb
GianES26 <a20224926@pucp.edu.pe>
2025-04-23 21:13:25 -0500

    Observaciones

--- TeleLink/src/main/java/com/example/telelink/repository/ObservacionRepository.java
+++ TeleLink/src/main/java/com/example/telelink/repository/ObservacionRepository.java
@@ -2,8 +2,22 @@ package com.example.telelink.repository;
 
 import com.example.telelink.entity.Observacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ObservacionRepository extends JpaRepository<Observacion, Integer> {
+
+    //List<Observacion> findByNivelUrgenciaOrderByNivelUrgenciaAsc(NivelUrgencia nivelUrgencia);
+
+    @Query(value = """
+            SELECT o.* FROM observaciones o \
+            JOIN espacios_deportivos e ON o.espacio_deportivo_id = e.espacio_deportivo_id \
+            JOIN establecimientos_deportivos ed ON e.establecimiento_deportivo_id = ed.establecimiento_deportivo_id \
+            JOIN usuarios c ON o.coordinador_id = c.usuario_id \
+            WHERE o.nivel_urgencia = :nivelUrgencia""", nativeQuery = true)
+    List<Observacion> findAllByNivelUrgenciaWithRelationsNative(Observacion.NivelUrgencia nivelUrgencia);
+
 }
--- TeleLink/src/main/java/com/example/telelink/repository/PagoRepository.java
+++ TeleLink/src/main/java/com/example/telelink/repository/PagoRepository.java
@@ -14,4 +14,5 @@ public interface PagoRepository extends JpaRepository<Pago, Integer> {
 
 
 
+
 }
