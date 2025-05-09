773711c9a06de3f7ce11048f068d18a419b9217d
OhMondayMonday <a20216352@pucp.edu.pe>
2025-05-08 20:34:45 -0500

    HTMLs Proyecto

--- TeleLink/src/main/java/com/example/telelink/controller/SuperadminController.java
+++ TeleLink/src/main/java/com/example/telelink/controller/SuperadminController.java
@@ -1,7 +1,14 @@
 package com.example.telelink.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/superadmin")
 public class SuperadminController {
+
+    @GetMapping("")
+    public String listarUsuarios() {
+        return "Superadmin/Dashboard";
+    }
 }
